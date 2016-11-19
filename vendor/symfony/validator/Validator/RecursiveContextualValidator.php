51910d4c4d9b584f3fea342861d81900a00ea515
Pascual Cano <pascual@strata3.com>
2016-11-19 22:20:52 +0100

    Drupal 8.2.3

--- vendor/symfony/validator/Validator/RecursiveContextualValidator.php
+++ vendor/symfony/validator/Validator/RecursiveContextualValidator.php
@@ -33,8 +33,6 @@ use Symfony\Component\Validator\Util\PropertyPath;
 /**
  * Recursive implementation of {@link ContextualValidatorInterface}.
  *
- * @since  2.5
- *
  * @author Bernhard Schussek <bschussek@gmail.com>
  */
 class RecursiveContextualValidator implements ContextualValidatorInterface
@@ -792,7 +790,7 @@ class RecursiveContextualValidator implements ContextualValidatorInterface
      * @param int                       $traversalStrategy The strategy used for
      *                                                     traversing the value
      * @param GroupSequence             $groupSequence     The group sequence
-     * @param string[]|null             $cascadedGroup     The group that should
+     * @param string|null               $cascadedGroup     The group that should
      *                                                     be passed to cascaded
      *                                                     objects instead of
      *                                                     the group sequence
